8cd6f83f5ec7f33164f472412258b32f04d13934
Leonardo <leonardo0507.henrique@gmail.com>
2023-01-08 15:34:16 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./scss/style.scss";
+
+const App = () => {
+    return (
+        <div>
+            <h1>My Title!</h1>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos aliquam ipsum dolores praesentium aut accusantium fugit beatae facilis! Exercitationem quia earum adipisci quasi cumque tempore soluta facere esse ea enim.
+            </p>
+
+            <button>
+                My button!
+            </button>
+        </div>
+    );
+};
+
+export default App;
